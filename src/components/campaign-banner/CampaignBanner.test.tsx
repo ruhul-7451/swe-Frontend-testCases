c7dcbe0a9490320c6e297eb7fddae9425eed44f4
Md. Ruhul Amin Khan <86850484+ruhul-7451@users.noreply.github.com>
2023-10-21 02:12:24 +0600

    added tests for game-box and modified campaign tests

--- src/components/campaign-banner/CampaignBanner.test.tsx
+++ src/components/campaign-banner/CampaignBanner.test.tsx
@@ -24,20 +24,21 @@ describe('[GAME] CampaignBanner', () => {
     });
     expect(campaignBanner).toHaveTextContent('Score 200 points and get 10% discount on your next mobile Recharge!');
   });
-
+  
   it('SHOULD Render CampaignBanner and maintain responsiveness WHEN viewport width is 250 px', () => {
-    window.innerWidth = 250;
+    global.innerWidth = 250;
     const { getByTestId } = render(<CampaignBanner />);
     const campaignBanner = getByTestId(TestElement.CAMPAIGN_BANNER);
-
     expect(campaignBanner).toBeInTheDocument();
-    // Add assertions for responsiveness, e.g., font size and width
-    // expect(campaignBanner).toHaveStyle({ fontSize: '12px' });
-    // expect(campaignBanner).toHaveStyle({ width: '70%' });
-     // Adjust to match your responsive design
-     const computedStyles = getComputedStyle(campaignBanner);
-     expect(computedStyles.fontSize).toBe('12px');
-     expect(computedStyles.width).toBe('70%');
+    expect(campaignBanner).toHaveStyle({
+      display: 'flex',
+      position: 'absolute',
+      flexDirection: 'column',
+      textAlign: 'center',
+      fontSize: '12px',
+      width: '70%',
+    });
+
   });
 
   it('SHOULD Render CampaignBanner and maintain responsiveness WHEN viewport height is 600 px', () => {
--- src/components/game-box/GameBox.test.tsx
+++ src/components/game-box/GameBox.test.tsx
@@ -1,18 +1,57 @@
-import { cleanup } from '@testing-library/react';
+import React from 'react';
+import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { GameBox } from './GameBox';
+import { TestElement } from '../../enums/TestElement';
+import { gameConfig } from '../../config/game-config';
 
 afterEach(cleanup);
 
 describe('[GAME] GameBox', () => {
   it('SHOULD Render GameBox with width maxHeight and maxWidth WHEN browsers viewport width and height is 1000px', () => {
-    //
+    render(
+      <GameBox>
+        <div>Content goes here</div>
+      </GameBox>
+    );
+    const gameBox = screen.getByTestId(TestElement.GAME_BOX);
+    
+    expect(gameBox).toBeInTheDocument();
+    expect(gameBox).toHaveStyle({
+      width: `${gameConfig.container.maxWidth}px`,
+      height: `${gameConfig.container.maxHeight}px`,
+    });
   });
 
   it('SHOULD Render GameBox with width 300px WHEN browsers viewport width 300px and height remains 1000px', () => {
-    //
+    global.innerWidth = 300;
+    render(
+      <GameBox>
+        <div>Content goes here</div>
+      </GameBox>
+    );
+    const gameBox = screen.getByTestId(TestElement.GAME_BOX);
+    
+    expect(gameBox).toBeInTheDocument();
+    expect(gameBox).toHaveStyle({
+      width: '300px',
+      height: `${gameConfig.container.maxHeight}px`,
+    });
   });
 
   it('SHOULD Render GameBox with height 600px WHEN browsers viewport width 1000px and height is 600px', () => {
-    //
+    global.innerHeight = 600;
+    render(
+      <GameBox>
+        <div>Content goes here</div>
+      </GameBox>
+    );
+    const gameBox = screen.getByTestId(TestElement.GAME_BOX);
+    
+    expect(gameBox).toBeInTheDocument();
+    expect(gameBox).toHaveStyle({
+      width: `${gameConfig.container.maxWidth}px`,
+      height: '600px',
+    });
   });
 });
